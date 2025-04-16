cbf41e8b5980ad4857035e909696fcc8a8e34bb0
Deepmoina Boruah <deepmoinaboruah7@gmail.com>
2025-04-17 01:14:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['https://ecommerce-greencart-1.onrender.com'];
 
 app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
 
